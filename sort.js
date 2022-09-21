1bc3c061a4165e38f207c86077167b9c4e593bd7
georgiev098 <Georgiev.Daniel@outlook.com>
2022-09-21 10:45:07 +0200

    add recursiveSort to sort.js

--- sort.js
+++ sort.js
@@ -3,3 +3,13 @@ const sortLargeArray = (array) => {
 }
 
 module.exports = sortLargeArray;
+
+const recursiveSort = (array) => {
+    if (array.length === 1) {
+        return array;
+    }
+    const mid = Math.floor(array.length / 2);
+    const left = array.slice(0, mid);
+    const right = array.slice(mid);
+    return merge(recursiveSort(left), recursiveSort(right));
+}
